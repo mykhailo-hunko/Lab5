1d0d9d91ecd4f19d53339f22fc5e9b7a5094feb3
Михайло Гунько <mykhailo.hunko@nure.ua>
2020-11-18 19:01:08 +0200

    bad

--- Lab5/Form1.Designer.cs
+++ Lab5/Form1.Designer.cs
@@ -36,6 +36,15 @@
             this.subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.TeacherOn = new System.Windows.Forms.Button();
+            this.SubjectOn = new System.Windows.Forms.Button();
+            this.textSubject = new System.Windows.Forms.TextBox();
+            this.textTeacher = new System.Windows.Forms.TextBox();
+            this.Remove = new System.Windows.Forms.Button();
+            this.textIndex = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -103,11 +112,101 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "Предмет";
             // 
+            // TeacherOn
+            // 
+            this.TeacherOn.Location = new System.Drawing.Point(31, 316);
+            this.TeacherOn.Name = "TeacherOn";
+            this.TeacherOn.Size = new System.Drawing.Size(75, 23);
+            this.TeacherOn.TabIndex = 6;
+            this.TeacherOn.Text = "Добавить";
+            this.TeacherOn.UseVisualStyleBackColor = true;
+            this.TeacherOn.Click += new System.EventHandler(this.TeacherOn_Click);
+            // 
+            // SubjectOn
+            // 
+            this.SubjectOn.Location = new System.Drawing.Point(31, 378);
+            this.SubjectOn.Name = "SubjectOn";
+            this.SubjectOn.Size = new System.Drawing.Size(75, 23);
+            this.SubjectOn.TabIndex = 7;
+            this.SubjectOn.Text = "Добавить";
+            this.SubjectOn.UseVisualStyleBackColor = true;
+            this.SubjectOn.Click += new System.EventHandler(this.SubjectOn_Click);
+            // 
+            // textSubject
+            // 
+            this.textSubject.Location = new System.Drawing.Point(159, 319);
+            this.textSubject.Name = "textSubject";
+            this.textSubject.Size = new System.Drawing.Size(100, 20);
+            this.textSubject.TabIndex = 8;
+            // 
+            // textTeacher
+            // 
+            this.textTeacher.Location = new System.Drawing.Point(159, 381);
+            this.textTeacher.Name = "textTeacher";
+            this.textTeacher.Size = new System.Drawing.Size(100, 20);
+            this.textTeacher.TabIndex = 9;
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(340, 317);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.TabIndex = 10;
+            this.Remove.Text = "Добавить";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // textIndex
+            // 
+            this.textIndex.Location = new System.Drawing.Point(445, 320);
+            this.textIndex.Name = "textIndex";
+            this.textIndex.Size = new System.Drawing.Size(100, 20);
+            this.textIndex.TabIndex = 11;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(634, 378);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 12;
+            this.Save.Text = "Добавить";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(31, 12);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 13;
+            this.Start.Text = "Добавить";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+           
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(767, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.textIndex);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.textTeacher);
+            this.Controls.Add(this.textSubject);
+            this.Controls.Add(this.SubjectOn);
+            this.Controls.Add(this.TeacherOn);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -132,6 +231,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn subject;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button TeacherOn;
+        private System.Windows.Forms.Button SubjectOn;
+        private System.Windows.Forms.TextBox textSubject;
+        private System.Windows.Forms.TextBox textTeacher;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.TextBox textIndex;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Lab5/Form1.cs
+++ Lab5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Lab5
 {
@@ -33,7 +34,133 @@ namespace Lab5
                 MessageBox.Show("Ошибка. Правильно заполните поля!");
                 return;
             }
+            if(dataGridView1.ColumnCount == 0)
+            {
+                return;
+            }
+            if(list.Exists(x => x.name == teacher) && list.Exists(x => x.subject == subject))
+            {
+                MessageBox.Show("Такая пара значений уже есть!");
+            }
+            else
+            {
+                list.Add(new Person(teacher, subject));
+                ds.Tables[0].Rows.Add(teacher, subject);
+            }
             
         }
+
+        private void TeacherOn_Click(object sender, EventArgs e)
+        {
+            string s = textSubject.Text;
+            if (string.IsNullOrEmpty(s))
+            {
+                return;
+            }
+
+            var selectedUsers = (from user in list where user.subject == s select user.name).ToList();
+            string result = null;
+            foreach(string user in selectedUsers)
+            {
+                result += user + "\n";
+            }
+
+            if(result == null)
+            {
+                MessageBox.Show("Преподавателей не найдено!");
+            }
+            else
+            {
+                MessageBox.Show(result);
+            }
+        }
+
+        private void SubjectOn_Click(object sender, EventArgs e)
+        {
+            string s = textSubject.Text;
+            if (string.IsNullOrEmpty(s))
+            {
+                return;
+            }
+
+            var selectedUsers = (from user in list where user.subject == s select user.name).ToList();
+            string result = null;
+            foreach (string user in selectedUsers)
+            {
+                result += user + "\n";
+            }
+
+            if (result == null)
+            {
+                MessageBox.Show("Предметов не найдено!");
+            }
+            else
+            {
+                MessageBox.Show(result);
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if(list.Count == 0)
+            {
+                return;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "XML|*.xml";
+
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ds.Tables[0].WriteXml(sfd.FileName);
+                } catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = Convert.ToInt32(textIndex.Text) - 1;
+                string teacher = dataGridView1.Rows[index].Cells["Name"].Value.ToString();
+                string subject = dataGridView1.Rows[index].Cells["Subject"].Value.ToString();
+
+                var indexOf = list.FindIndex(x => x.name == teacher && x.subject == subject);
+                list.RemoveAt(indexOf);
+                ds.Tables[0].Rows.RemoveAt(index);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "XML|*.xml";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    XmlReader xmlFile = XmlReader.Create(ofd.FileName, new XmlReaderSettings());
+                    ds.ReadXml(xmlFile);
+                    dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                    dataGridView1.Rows.RemoveAt(0);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        
     }
 }
